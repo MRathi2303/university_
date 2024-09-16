5d47034ac7b3ba088d06f90f7f789349af25f16c
Moon Rathi <147794854+MRathi2303@users.noreply.github.com>
2024-09-16 16:16:42 +0530

    arrays

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a new number:");
+        int a = sc.nextInt();
+
+        System.out.println(a);
+    }
+}
