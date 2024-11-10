a48824caf9533adf11319aa517f7572bf6c0b248
Moon Rathi <147794854+MRathi2303@users.noreply.github.com>
2024-11-10 22:29:56 +0530

    abstarct class

---
+++ src/ExceptionHandling/ExceptionExample.java
@@ -0,0 +1,15 @@
+package ExceptionHandling;
+
+public class ExceptionExample {
+    public static void main(String[] args) {
+        try {
+            int num = 100 / 0;  // This will throw ArithmeticException
+        }
+//            catch (ArithmeticException e) {
+//            System.out.println("Error: Division by zero is not allowed.");
+//        }
+        finally {
+            System.out.println("This block is always executed.");
+        }
+    }
+}
